8bff84087621d1d5ce946bcf52e80d4fbcda9c19
reneeb <info@perl-services.de>
2018-11-24 20:08:53 +0100

    prepare release

--- lib/OData/QueryParams/DBIC.pm
+++ lib/OData/QueryParams/DBIC.pm
@@ -20,7 +20,7 @@ use Scalar::Util qw(blessed);
 
 our @EXPORT = qw(params_to_dbic);
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub params_to_dbic ( $query_string, %opts ) {
     my $query;
