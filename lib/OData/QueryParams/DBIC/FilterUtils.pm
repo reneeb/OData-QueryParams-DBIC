db91680a6fa61eec62c71d41158f06b7ae2f314f
reneeb <info@perl-services.de>
2018-12-05 15:29:20 +0100

    prepare release

--- lib/OData/QueryParams/DBIC/FilterUtils.pm
+++ lib/OData/QueryParams/DBIC/FilterUtils.pm
@@ -13,7 +13,7 @@ no warnings 'experimental::signatures';
 use parent 'Exporter';
 
 our @EXPORT_OK = qw(parser);
-our $VERSION   = '0.05';
+our $VERSION   = '0.06';
 
 use constant Operators => {
     EQUALS             => 'eq',
